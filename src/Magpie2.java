88a8dccaa6809a4dd7130f842c15f3fd4d3221eb
Jon Dixon <bkndenver0880@gmail.com>
2018-11-07 08:31:35 -0700

    finish activity 2

--- src/Magpie2.java
+++ src/Magpie2.java
@@ -8,14 +8,14 @@
  * Handles responding to simple words and phrases</li>
  * </ul>
  * This version uses a nested if to handle default responses.
- * 
+ *
  * @author Laurie White
  * @version April 2012
  */
 public class Magpie2 {
 	/**
 	 * Get a default greeting
-	 * 
+	 *
 	 * @return a greeting
 	 */
 	public String getGreeting() {
@@ -24,21 +24,61 @@ public class Magpie2 {
 
 	/**
 	 * Gives a response to a user statement
-	 * 
+	 *
 	 * @param statement
 	 *            the user statement
 	 * @return a response based on the rules given
 	 */
 	public String getResponse(String statement) {
 		String response = "";
-		if (statement.indexOf("no") >= 0) {
+		if (statement.indexOf("no") >= 0)
+				{
 			response = "Why so negative?";
-		} else if (statement.indexOf("mother") >= 0
+		}
+
+			else if (statement.indexOf("yes") >= 0
+				|| statement.indexOf("okay") >= 0)
+				{
+					response = "Okay... so?";
+		}
+
+			else if (statement.indexOf("song") >= 0)
+				{
+					response = "What's your favorite song?";
+		}
+
+			else if (statement.indexOf("music") >= 0)
+				{
+					response = "What's your favoite genre of music?";
+		}
+
+			else if (statement.indexOf("mother") >= 0
 				|| statement.indexOf("father") >= 0
 				|| statement.indexOf("sister") >= 0
-				|| statement.indexOf("brother") >= 0) {
+				|| statement.indexOf("brother") >= 0
+				|| statement.indexOf("mom") >= 0
+				|| statement.indexOf("dad") >= 0)
+				{
 			response = "Tell me more about your family.";
-		} else {
+		}
+
+			else if (statement.indexOf("cat") >= 0
+				|| statement.indexOf("dog") >= 0)
+				{
+			response = "Tell me more about your pet.";
+		}
+
+			else if (statement.indexOf("Mr. Padjen") >= 0)
+				{
+			response = "I like that teacher!";
+		}
+
+			else if (statement.length() <= 0)
+				{
+					response = "Please say something...";
+		}
+
+			else {
 			response = getRandomResponse();
 		}
 		return response;
@@ -46,24 +86,40 @@ public class Magpie2 {
 
 	/**
 	 * Pick a default response to use if nothing else fits.
-	 * 
+	 *
 	 * @return a non-committal string
 	 */
 	private String getRandomResponse() {
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
 		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
 		String response = "";
 
-		if (whichResponse == 0) {
+		if (whichResponse == 0)
+		{
 			response = "Interesting, tell me more.";
-		} else if (whichResponse == 1) {
+		}
+		else if (whichResponse == 1)
+		{
 			response = "Hmmm.";
-		} else if (whichResponse == 2) {
+		}
+		else if (whichResponse == 2)
+		{
 			response = "Do you really think so?";
-		} else if (whichResponse == 3) {
+		}
+		else if (whichResponse == 3)
+		{
 			response = "You don't say.";
 		}
+		else if (whichResponse == 4)
+		{
+			response = "That's cool, tell me more.";
+		}
+		else if (whichResponse == 5)
+		{
+			response = "I'd love to hear more about it.";
+		}
+
 
 		return response;
 	}
--- src/MagpieRunner2.java
+++ src/MagpieRunner2.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 /**
  * A simple class to run the Magpie class.
- * 
+ *
  * @author Laurie White
  * @version April 2012
  */
@@ -15,12 +15,12 @@ public class MagpieRunner2 {
 		Magpie2 maggie = new Magpie2();
 
 		System.out.println(maggie.getGreeting());
-		Scanner in = new Scanner(System.in);
-		String statement = in.nextLine();
+		Scanner input = new Scanner(System.in);
+		String statement = input.nextLine();
 
 		while (!statement.equals("Bye")) {
 			System.out.println(maggie.getResponse(statement));
-			statement = in.nextLine();
+			statement = input.nextLine();
 		}
 	}
 
